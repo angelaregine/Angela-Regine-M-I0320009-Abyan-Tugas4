e540568a7f4bd78db31571b0f930b6ff0eb2d49b
Angela R <angela.mutiaraputi@gmail.com>
2021-03-26 15:40:36 +0700

    Pengerjaan soal

--- I0320009_soal6_tugas4.py
+++ I0320009_soal6_tugas4.py
@@ -1,18 +1,18 @@
-a = 4
-b = 11
+a = 4               # 4  = 0100
+b = 11              # 11 = 1011
 c = 0
 
-c = a | b
+c = a | b           # 15 = 1111
 print("Hasil dari 4 | 11 adalah", c)
 
-c = a >> b
+c = a >> b          # 0  = 0000
 print("Hasil dari 4 >> 11 adalah", c)
 
-c = a ^ b
+c = a ^ b           # 15 = 1111
 print("Hasil dari 4 ^ 11 adalah", c)
 
-c = ~a
+c = ~a              # -5 = 0101
 print("Hasil dari ~4 adalah", c)
 
-c = b & a
+c = b & a           # 0 =  0000
 print("Hasil dari 11 & 4 adalah", c)
